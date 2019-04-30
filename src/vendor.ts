290155fe182b690ec17f1912de841a06c9ce4b2b
William Perez <wjph.dev@gmail.com>
2019-04-30 10:04:19 -0400

    addin scrollable directive

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,4 +1,5 @@
 
 import './modernizr.js'; // 'npm run modernizr' to create this file
 
-// import '../node_modules/jquery-slimscroll/jquery.slimscroll.js';
+
+import '../node_modules/jquery-slimscroll/jquery.slimscroll.js';
