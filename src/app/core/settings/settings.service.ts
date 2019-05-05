95c88871db44c59607e219153baed09d719d1ffc
William Perez <wjph.dev@gmail.com>
2019-05-05 17:38:40 -0400

    _

--- src/app/core/settings/settings.service.ts
+++ src/app/core/settings/settings.service.ts
@@ -20,7 +20,7 @@ export class SettingsService {
         // App Settings
         // -----------------------------------
         this.app = {
-            name: 'Angle',
+            name: 'DOCTORS HEALTHCARE PLANS',
             description: 'Angular Bootstrap Admin Template',
             year: ((new Date()).getFullYear())
         };
