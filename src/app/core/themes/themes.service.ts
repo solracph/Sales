5afc3b0a44eb023f8062559f1f649d630a351c51
William Perez <wjph.dev@gmail.com>
2019-05-09 09:16:05 -0400

    default theme to A

--- src/app/core/themes/themes.service.ts
+++ src/app/core/themes/themes.service.ts
@@ -13,7 +13,7 @@ const themeH = require('../../shared/styles/themes/theme-h.scss');
 export class ThemesService {
 
     styleTag: any;
-    defaultTheme: string = 'H';
+    defaultTheme: string = 'A';
 
     constructor() {
         this.createStyle();
